f25e83c8e524322492f4eb19b3e65258e79c4a9d
kckc0608 <kckc0608@gmail.com>
2023-05-14 13:23:49 +0900

    FEAT: Add ListView

--- lib/page/search_sheet.dart
+++ lib/page/search_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class SearchSheet extends StatefulWidget {
   const SearchSheet({Key? key}) : super(key: key);
@@ -10,6 +11,19 @@ class SearchSheet extends StatefulWidget {
 class _SearchSheetState extends State<SearchSheet> {
   @override
   Widget build(BuildContext context) {
-    return const Text("hi");
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: ListView.separated(
+          itemBuilder: (context, idx) {
+            return Text(idx.toString());
+          },
+          separatorBuilder: (context, idx) {
+            return const Divider(
+              height: 4.0,
+              thickness: 1.0,
+            );
+          },
+          itemCount: 100),
+    );
   }
 }
