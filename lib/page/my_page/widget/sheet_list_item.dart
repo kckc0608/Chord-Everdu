ee70f7f68ce5ed14b63e9505e8c544338e1395c4
kckc0608 <kckc0608@gmail.com>
2023-06-30 22:58:45 +0900

    feat: 악보 삭제 기능 추가 & 마이페이지 UI 수정

--- lib/page/my_page/widget/sheet_list_item.dart
+++ lib/page/my_page/widget/sheet_list_item.dart
@@ -1,5 +1,6 @@
 import 'package:chord_everdu/data_class/sheet.dart';
 import 'package:chord_everdu/page/sheet_viewer/sheet_viewer.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -22,7 +23,7 @@ class SheetListItem extends StatelessWidget {
         }));
       },
       child: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
         child: Row(
           children: [
             Expanded(
@@ -35,7 +36,7 @@ class SheetListItem extends StatelessWidget {
               ],
             )),
             IconButton(
-              icon: const Icon(Icons.edit),
+              icon: const Icon(Icons.edit_outlined),
               onPressed: () {
                 context.read<Sheet>().isReadOnly = false;
                 Navigator.of(context).push(MaterialPageRoute(builder: (context) {
@@ -43,13 +44,40 @@ class SheetListItem extends StatelessWidget {
                 }));
               }),
             IconButton(
-                icon: const Icon(Icons.delete),
+                icon: const Icon(Icons.delete_outline),
                 color: Colors.redAccent,
                 onPressed: () {
-                  context.read<Sheet>().isReadOnly = false;
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-                    return SheetViewer(sheetID: sheetID);
-                  }));
+                  showDialog(context: context, builder: (context) => AlertDialog(
+                    content: const Text("선택한 악보를 정말로 삭제하시겠습니까?"),
+                    actions: [
+                      TextButton(onPressed: () {
+                        Navigator.of(context).pop();
+                      }, child: const Text("취소")),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.redAccent
+                        ),
+                        onPressed: () {
+                          FirebaseFirestore.instance.collection('sheet_list')
+                              .doc(sheetID).delete().then((value) {
+                            Navigator.of(context).pop();
+                            showDialog(
+                              context: context,
+                              builder: (context) => AlertDialog(
+                                content: const Text("삭제가 완료 되었습니다."),
+                                actions: [
+                                  ElevatedButton(onPressed: () {
+                                    Navigator.of(context).pop();
+                                  }, child: const Text("확인"))
+                                ],
+                              ),
+                            );
+                          });
+                        },
+                        child: const Text("삭제"),
+                      ),
+                    ],
+                  ),);
                 }),
           ],
         ),
