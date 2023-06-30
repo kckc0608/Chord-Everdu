c2b503d9ae3206d80766bcdd53bfaa9c69d08d1d
kckc0608 <kckc0608@gmail.com>
2023-06-30 21:57:21 +0900

    feat: my page UI 수정

--- lib/page/my_page/widget/sheet_list_item.dart
+++ lib/page/my_page/widget/sheet_list_item.dart
@@ -1,5 +1,7 @@
+import 'package:chord_everdu/data_class/sheet.dart';
 import 'package:chord_everdu/page/sheet_viewer/sheet_viewer.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SheetListItem extends StatelessWidget {
   final String sheetID, title, singer;
@@ -14,6 +16,7 @@ class SheetListItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {
+        //context.read<Sheet>().isReadOnly = true;
         Navigator.of(context).push(MaterialPageRoute(builder: (context) {
           return SheetViewer(sheetID: sheetID);
         }));
@@ -30,7 +33,24 @@ class SheetListItem extends StatelessWidget {
                   const SizedBox(height: 4),
                   Text(singer, style: const TextStyle(color: Colors.black54),),
               ],
-            ))
+            )),
+            IconButton(
+              icon: const Icon(Icons.edit),
+              onPressed: () {
+                //context.read<Sheet>().isReadOnly = false;
+                Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+                  return SheetViewer(sheetID: sheetID);
+                }));
+              }),
+            IconButton(
+                icon: const Icon(Icons.delete),
+                color: Colors.redAccent,
+                onPressed: () {
+                  //context.read<Sheet>().isReadOnly = false;
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+                    return SheetViewer(sheetID: sheetID);
+                  }));
+                }),
           ],
         ),
       ),
