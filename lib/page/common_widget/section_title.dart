bdbcbf611f6e224383629579631c59b1a6f0efc4
kckc0608 <kckc0608@gmail.com>
2023-08-01 13:09:11 +0900

    DESIGN: 모달 디자인 통일
    1. TextTheme 커스텀 디자인을 title medium 대신 display medium 에 적용
      - section title 디자인 display medium을 사용하도록 수정
    2. 모달 디자인 통일
      - 새 악보 모달의 드롭박스 디자인 개선
      - 모달 세로 길이를 고정하지 않고 content fit 하게 수정

--- lib/page/common_widget/section_title.dart
+++ lib/page/common_widget/section_title.dart
@@ -8,7 +8,7 @@ class SectionTitle extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 8),
-      child: Text(data, style: Theme.of(context).textTheme.titleMedium),
+      child: Text(data, style: Theme.of(context).textTheme.displayLarge),
     );
   }
 }
